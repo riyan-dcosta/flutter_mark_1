f1e71174255cb2d4a426188f3054dd1117e1fa9d
riyan-dcosta <rdriyan@gmail.com>
2023-08-07 09:51:41 +0530

    union/sealed class in freezed

--- lib/features/http_album/domain/entities/album_model.freezed.dart
+++ lib/features/http_album/domain/entities/album_model.freezed.dart
@@ -177,3 +177,391 @@ abstract class _Album implements Album {
   _$$_AlbumCopyWith<_$_Album> get copyWith =>
       throw _privateConstructorUsedError;
 }
+
+/// @nodoc
+mixin _$AlbumState {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() loading,
+    required TResult Function() loaded,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function()? loading,
+    TResult? Function()? loaded,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? loading,
+    TResult Function()? loaded,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(Initial value) initial,
+    required TResult Function(_AlbbumLoading value) loading,
+    required TResult Function(_AlbumLoaded value) loaded,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(Initial value)? initial,
+    TResult? Function(_AlbbumLoading value)? loading,
+    TResult? Function(_AlbumLoaded value)? loaded,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(Initial value)? initial,
+    TResult Function(_AlbbumLoading value)? loading,
+    TResult Function(_AlbumLoaded value)? loaded,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $AlbumStateCopyWith<$Res> {
+  factory $AlbumStateCopyWith(
+          AlbumState value, $Res Function(AlbumState) then) =
+      _$AlbumStateCopyWithImpl<$Res, AlbumState>;
+}
+
+/// @nodoc
+class _$AlbumStateCopyWithImpl<$Res, $Val extends AlbumState>
+    implements $AlbumStateCopyWith<$Res> {
+  _$AlbumStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+}
+
+/// @nodoc
+abstract class _$$InitialCopyWith<$Res> {
+  factory _$$InitialCopyWith(_$Initial value, $Res Function(_$Initial) then) =
+      __$$InitialCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$InitialCopyWithImpl<$Res>
+    extends _$AlbumStateCopyWithImpl<$Res, _$Initial>
+    implements _$$InitialCopyWith<$Res> {
+  __$$InitialCopyWithImpl(_$Initial _value, $Res Function(_$Initial) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$Initial implements Initial {
+  const _$Initial();
+
+  @override
+  String toString() {
+    return 'AlbumState.initial()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$Initial);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() loading,
+    required TResult Function() loaded,
+  }) {
+    return initial();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function()? loading,
+    TResult? Function()? loaded,
+  }) {
+    return initial?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? loading,
+    TResult Function()? loaded,
+    required TResult orElse(),
+  }) {
+    if (initial != null) {
+      return initial();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(Initial value) initial,
+    required TResult Function(_AlbbumLoading value) loading,
+    required TResult Function(_AlbumLoaded value) loaded,
+  }) {
+    return initial(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(Initial value)? initial,
+    TResult? Function(_AlbbumLoading value)? loading,
+    TResult? Function(_AlbumLoaded value)? loaded,
+  }) {
+    return initial?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(Initial value)? initial,
+    TResult Function(_AlbbumLoading value)? loading,
+    TResult Function(_AlbumLoaded value)? loaded,
+    required TResult orElse(),
+  }) {
+    if (initial != null) {
+      return initial(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class Initial implements AlbumState {
+  const factory Initial() = _$Initial;
+}
+
+/// @nodoc
+abstract class _$$_AlbbumLoadingCopyWith<$Res> {
+  factory _$$_AlbbumLoadingCopyWith(
+          _$_AlbbumLoading value, $Res Function(_$_AlbbumLoading) then) =
+      __$$_AlbbumLoadingCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$_AlbbumLoadingCopyWithImpl<$Res>
+    extends _$AlbumStateCopyWithImpl<$Res, _$_AlbbumLoading>
+    implements _$$_AlbbumLoadingCopyWith<$Res> {
+  __$$_AlbbumLoadingCopyWithImpl(
+      _$_AlbbumLoading _value, $Res Function(_$_AlbbumLoading) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$_AlbbumLoading implements _AlbbumLoading {
+  const _$_AlbbumLoading();
+
+  @override
+  String toString() {
+    return 'AlbumState.loading()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$_AlbbumLoading);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() loading,
+    required TResult Function() loaded,
+  }) {
+    return loading();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function()? loading,
+    TResult? Function()? loaded,
+  }) {
+    return loading?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? loading,
+    TResult Function()? loaded,
+    required TResult orElse(),
+  }) {
+    if (loading != null) {
+      return loading();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(Initial value) initial,
+    required TResult Function(_AlbbumLoading value) loading,
+    required TResult Function(_AlbumLoaded value) loaded,
+  }) {
+    return loading(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(Initial value)? initial,
+    TResult? Function(_AlbbumLoading value)? loading,
+    TResult? Function(_AlbumLoaded value)? loaded,
+  }) {
+    return loading?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(Initial value)? initial,
+    TResult Function(_AlbbumLoading value)? loading,
+    TResult Function(_AlbumLoaded value)? loaded,
+    required TResult orElse(),
+  }) {
+    if (loading != null) {
+      return loading(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _AlbbumLoading implements AlbumState {
+  const factory _AlbbumLoading() = _$_AlbbumLoading;
+}
+
+/// @nodoc
+abstract class _$$_AlbumLoadedCopyWith<$Res> {
+  factory _$$_AlbumLoadedCopyWith(
+          _$_AlbumLoaded value, $Res Function(_$_AlbumLoaded) then) =
+      __$$_AlbumLoadedCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$_AlbumLoadedCopyWithImpl<$Res>
+    extends _$AlbumStateCopyWithImpl<$Res, _$_AlbumLoaded>
+    implements _$$_AlbumLoadedCopyWith<$Res> {
+  __$$_AlbumLoadedCopyWithImpl(
+      _$_AlbumLoaded _value, $Res Function(_$_AlbumLoaded) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$_AlbumLoaded implements _AlbumLoaded {
+  const _$_AlbumLoaded();
+
+  @override
+  String toString() {
+    return 'AlbumState.loaded()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$_AlbumLoaded);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() loading,
+    required TResult Function() loaded,
+  }) {
+    return loaded();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function()? loading,
+    TResult? Function()? loaded,
+  }) {
+    return loaded?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? loading,
+    TResult Function()? loaded,
+    required TResult orElse(),
+  }) {
+    if (loaded != null) {
+      return loaded();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(Initial value) initial,
+    required TResult Function(_AlbbumLoading value) loading,
+    required TResult Function(_AlbumLoaded value) loaded,
+  }) {
+    return loaded(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(Initial value)? initial,
+    TResult? Function(_AlbbumLoading value)? loading,
+    TResult? Function(_AlbumLoaded value)? loaded,
+  }) {
+    return loaded?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(Initial value)? initial,
+    TResult Function(_AlbbumLoading value)? loading,
+    TResult Function(_AlbumLoaded value)? loaded,
+    required TResult orElse(),
+  }) {
+    if (loaded != null) {
+      return loaded(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _AlbumLoaded implements AlbumState {
+  const factory _AlbumLoaded() = _$_AlbumLoaded;
+}
